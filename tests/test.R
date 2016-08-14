a609c71c2ed23f674640da3a428079da4c21fee3
J27 <jean.d.marchal@gmail.com>
2016-08-14 12:25:44 +0200

    [SizePredict] Update test

--- tests/test.R
+++ tests/test.R
@@ -16,20 +16,20 @@ library(SpaDES)
     # )
 
   ## 6 var
-    mySim <- simInit(
-      times = list(start = 1, end = 1, timeunit = "year"),
-      modules = list("fireSense_SizePredict"),
-      paths = list(modulePath = " # replace with empty string instead"),
-      params = list(fireSense_SizePredict = list(data = c("dataFireSense_Size", "dataFireSense_Size2"))),
-      inputs = data.frame(
-        files = c("Z:/fireSense_SizeFitted_6v.rds", "Z:/dataFireSense_Size.rds"),
-        functions = c("readRDS", "readRDS"),
-        package = c("base", "base"),
-        objectName = c("fireSense_SizeFitted", NA),
-        stringsAsFactors = FALSE)
-    )
-    mySim$dataFireSense_Size2 <- dplyr::select(mySim$dataFireSense_Size, MDC_JUN)
-    mySim$dataFireSense_Size <- dplyr::select(mySim$dataFireSense_Size, -MDC_JUN)
+    # mySim <- simInit(
+    #   times = list(start = 1, end = 1, timeunit = "year"),
+    #   modules = list("fireSense_SizePredict"),
+    #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_SizePredict = list(data = c("dataFireSense_Size", "dataFireSense_Size2"))),
+    #   inputs = data.frame(
+    #     files = c("Z:/fireSense_SizeFitted_6v.rds", "Z:/dataFireSense_Size.rds"),
+    #     functions = c("readRDS", "readRDS"),
+    #     package = c("base", "base"),
+    #     objectName = c("fireSense_SizeFitted", NA),
+    #     stringsAsFactors = FALSE)
+    # )
+    # mySim$dataFireSense_Size2 <- dplyr::select(mySim$dataFireSense_Size, MDC_JUN)
+    # mySim$dataFireSense_Size <- dplyr::select(mySim$dataFireSense_Size, -MDC_JUN)
 
 ## RasterLayer
   ## 1 var
@@ -37,8 +37,9 @@ library(SpaDES)
     #   times = list(start = 1, end = 1, timeunit = "year"),
     #   modules = list("fireSense_SizePredict"),
     #   paths = list(modulePath = " # replace with empty string instead"),
+    #   params = list(fireSense_SizePredict = list(mapping = c(MDC_MJ = "MDC_JUN"))),
     #   inputs = data.frame(
-    #     files = c("Z:/fireSense_SizeFitted1var.rds", "Z:/MDC_JUN.tif"),
+    #     files = c("Z:/fireSense_SizeFitted_1v.rds", "Z:/MDC_JUN.tif"),
     #     functions = c("readRDS", "raster"),
     #     package = c("base", "raster"),
     #     objectName = c("fireSense_SizeFitted", NA),
@@ -46,17 +47,17 @@ library(SpaDES)
     # )
 
   ## 6 var
-    # mySim <- simInit(
-    #   times = list(start = 1, end = 1, timeunit = "year"),
-    #   modules = list("fireSense_SizePredict"),
-    #   paths = list(modulePath = " # replace with empty string instead"),
-    #   inputs = data.frame(
-    #     files = c("Z:/fireSense_SizeFitted6var.rds", "Z:/MDC_JUN.tif", "Z:/MDC_JUN.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif"),
-    #     functions = c("readRDS", "raster", "raster", "raster", "raster", "raster", "raster"),
-    #     package = c("base", "raster", "raster", "raster", "raster", "raster", "raster"),
-    #     objectName = c("fireSense_SizeFitted", "MDC_JUN", "MDC_MJ", "HW", "DIST", "O", "WATER"),
-    #     stringsAsFactors = FALSE)
-    # )
+    mySim <- simInit(
+      times = list(start = 1, end = 1, timeunit = "year"),
+      modules = list("fireSense_SizePredict"),
+      paths = list(modulePath = " # replace with empty string instead"),
+      inputs = data.frame(
+        files = c("Z:/fireSense_SizeFitted_6v.rds", "Z:/MDC_JUN.tif", "Z:/MDC_JUN.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif", "Z:/HW.tif"),
+        functions = c("readRDS", "raster", "raster", "raster", "raster", "raster", "raster"),
+        package = c("base", "raster", "raster", "raster", "raster", "raster", "raster"),
+        objectName = c("fireSense_SizeFitted", "MDC_JUN", "MDC_MJ", "HW", "DIST", "O", "WATER"),
+        stringsAsFactors = FALSE)
+    )
 
 spades(mySim, debug = FALSE)
 
